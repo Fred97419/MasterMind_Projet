25a73b2462ee5ccb5d98adb76ec83d7666236b04
fred_fabre_ferber <fred_fabre_ferber@bitbucket.org>
2019-03-25 14:26:45 +0400

    Initial commit

---
+++ app/src/main/java/com/fred/mastermind_projet/Mastermind.java
@@ -0,0 +1,27 @@
+package com.fred.mastermind_projet;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Mastermind extends View {
+
+
+
+    public Mastermind(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+
+    private void initJeu(){
+
+
+
+
+
+    }
+
+
+
+}
